3df01ad1ea0c90127db4c31ef809f7d8f9e8e2d1
Vincent Agnano <vincent.agnano@scopyleft.fr>
2013-04-17 13:54:16 +0200

    code refactoring

--- siret.js
+++ siret.js
@@ -2,68 +2,51 @@ var casper = require('casper').create()
   , query = casper.cli.options.code 
   , url = 'https://www.google.fr/#q='+query+'+site:www.bilansgratuits.fr'
   , info = {}
-  , link = null
+  , link = null;
 
 
 function getLinks() {
-  var links = document.querySelectorAll('h3.r a')
-  return Array.prototype.map.call(links, function(e) {
-    return e.getAttribute('href')
-  })
+  var links = document.querySelectorAll('h3.r a');
+  if(links.length) {
+    return links[0].getAttribute('href');
+  }
+  return '';
 }
 
-casper
-.start()
-.thenOpen(url, function(){
+casper.start(url, function() {
+  var href = this.evaluate(getLinks);
 
-  var links = this.evaluate(getLinks)
-    , match = null
-
-  //match link
-  for(var i=0; i< links.length; i++){
-    match = (links[i].split('/url?q=')[1]).split('&')[0]
-    if(/.*bilansgratuits.*/.test(match)){
-      link = match
-      break
-    }
+  if(href) {
+    link = (href.split('/url?q=')[1]).split('&')[0];
+    this.open(link);
   }
+});
 
-  if(link) this.open(link)
+casper.then(function() {
+  if(this.exists('#siret')) {
+    var zipCity = this.fetchText('#fiche-societe > div:nth-child(1) > h3:nth-child(3)').split(' ') // 13100 AIX CEDEX
+      , dataSelector = function(row, col) {return '#infogene-societe table:nth-of-type(1) tr:nth-of-type('+row+') td:nth-of-type('+col+')'}
+      , companyInfos = this.fetchText(dataSelector(4, 1)).replace(/\n/g, '').replace(/\t/g, '').split(':');
 
-})
-
-.then(function() {
-  
-  if(this.exists('#siret')){
-
-    var a,b
-
-    info.name     = this.fetchText('#fiche-societe > div:nth-child(1) > h1:nth-child(1)')
-    info.address  = this.fetchText('#fiche-societe > div:nth-child(1) > h3:nth-child(2)')
-
-    a = this.fetchText('#fiche-societe > div:nth-child(1) > h3:nth-child(3)').split(' ') // 13100 AIX CEDEX
-    info.zipcode  = a[0]
-    a.splice(0,1)
-    info.city     = a.join(' ')
-
-    a = this.fetchText('#infogene-societe table:nth-of-type(1) tr:nth-of-type(4) td:nth-of-type(1)').replace(/\n/g, '').replace(/\t/g, '').split(':')
-    info.code     = a[0].substr(0, a[0].length -1)
-    info.desc     = a[1].substr(1, a[1].length)
-    
-    info.vat      = this.getHTML('#infogene-societe table:nth-of-type(1) tr:nth-of-type(6) td:nth-of-type(1)').replace(/\n/g, '').replace(/\t/g, '')
-    info.siret    = this.fetchText('#siret').split('siret: ')[1]
-    info.type     = this.getHTML('#infogene-societe table:nth-of-type(1) tr:nth-of-type(1) td:nth-of-type(1)').replace(/\n/g, '').replace(/\t/g, '')
-    info.capital  = this.getHTML('#infogene-societe table:nth-of-type(1) tr:nth-of-type(2) td:nth-of-type(1)').replace(/\n/g, '').replace(/\t/g, '')
-    info.date     = this.getHTML('#infogene-societe table:nth-of-type(1) tr:nth-of-type(2) td:nth-of-type(2)').replace(/\n/g, '').replace(/\t/g, '')
-    info.phone    = this.getHTML('#infogene-societe table:nth-of-type(1) tr:nth-of-type(5) td:nth-of-type(1)').replace(/\n/g, '').replace(/\t/g, '').replace(/ /g, '')
-    info.link     = link
-
+    info = {
+      name    : this.fetchText('#fiche-societe > div:nth-child(1) > h1:nth-child(1)'),
+      address : this.fetchText('#fiche-societe > div:nth-child(1) > h3:nth-child(2)'),
+      zipcode : zipCity[0],
+      city    : zipCity.splice(0,1) && zipCity.join(' '),
+      code    : companyInfos[0].substr(0, companyInfos[0].length -1),
+      desc    : companyInfos[1].substr(1, companyInfos[1].length),
+      vat     : this.getHTML(dataSelector(6, 1)).replace(/\n/g, '').replace(/\t/g, ''),
+      siret   : this.fetchText('#siret').split('siret: ')[1],
+      type    : this.getHTML(dataSelector(1, 1)).replace(/\n/g, '').replace(/\t/g, ''),
+      capital : this.getHTML(dataSelector(2, 1)).replace(/\n/g, '').replace(/\t/g, ''),
+      date    : this.getHTML(dataSelector(2, 2)).replace(/\n/g, '').replace(/\t/g, ''),
+      phone   : this.getHTML(dataSelector(5, 1)).replace(/\n/g, '').replace(/\t/g, '').replace(/ /g, ''),
+      link    : link
+    };
   }
-
-})
-
+});
 
 casper.run(function(){
-  require('utils').dump(info)
-  this.exit()
+  require('utils').dump(info);
+  this.exit();
 });
